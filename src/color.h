ca37515a5c164ed906f14967728a97d02af39d23
Michael Junges <99703216+Kaeljun@users.noreply.github.com>
2024-07-12 22:48:04 -0300

    feat: adds ray class and can now render a linear gradient

--- src/color.h
+++ src/color.h
@@ -3,11 +3,11 @@
 
 #include "vec3.h"
 
-#include <iostream>
+#include <fstream>
 
 using color = vec3;
 
-void write_color(std::ostream &out, color pixel_color) {
+void write_color(std::ofstream &out, color pixel_color) {
     // Write the translated [0,255] value of each color component.
     out << static_cast<int>(255.999 * pixel_color.x()) << ' '
         << static_cast<int>(255.999 * pixel_color.y()) << ' '
